293834172de3558d8acebd8fc27c866db4841cc8
Giorgio Pizzorni <giorgio@pizzorni.com>
2016-03-07 15:48:58 -0500

    IT WOOOORKS

--- animation.py
+++ animation.py
@@ -1,24 +1,47 @@
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import numpy as np
 
-
-fig = plt.figure()
-#ax = fig.add_axes(frameon = False)
-#ax.set_xlim(0,dim), ax.set_xticks([])
-#ax.set_ylim(0,dim), ax.set_yticks([])
 
 f = open('test.out')
 frames = f.read().split('\n')
+f.close()
+
 info = frames[0].split(':')
 gen = int(info[0])
 dim = int(info[1])
-f.close()
+
+frames.remove(frames[0])
+frames.remove('')
+
+#chunk frames into individual generations
+chunked_frames = [frames[i:i+dim] for i in 
+                        xrange(0, len(frames), dim)] 
 
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim(0,dim), ax.set_xticks([])
+ax.set_ylim(0,dim), ax.set_yticks([])
+peng_scat = ax.scatter(None, None, color = 'green')
+bear_scat = ax.scatter(None, None, color = 'red')
 
-#def update(frame_number):
- # print "stuff"
 
+def update(frame_number):
+  frame = chunked_frames[frame_number]
+  penguins = []
+  bears = []
+  for r in range(dim):
+    for c in range(dim):
+      species = frame[r][c]
+      if species == '1':
+        penguins.append((r,c))
+      elif species == '2':
+        bears.append((r,c))
+  peng_scat.set_offsets(penguins)
+  bear_scat.set_offsets(bears)
+  label = "Generation: " + str(frame_number)
+  plt.suptitle(label)
 
-#animation = FuncAnimation(fig, update, interval = 10)
-#plt.show()
+animation = FuncAnimation(fig, update, frames = gen, interval = 100)
+plt.show()
